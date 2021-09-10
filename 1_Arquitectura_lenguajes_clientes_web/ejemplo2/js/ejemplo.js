02dc2801eb6ec07c4d1583aa3b5a599562262655
Inma Gijón <igijoninf@gmail.com>
2021-09-10 22:19:57 +0200

    Ejemplos unidad 1

---
+++ 1_Arquitectura_lenguajes_clientes_web/ejemplo2/js/ejemplo.js
@@ -0,0 +1,13 @@
+function Funcion() {
+  //declaramos una variable
+  var x = 3;
+  /* Los comentarios pueden ser
+también multilínea. Declaramos
+varias variables */
+  var y = 5;
+  var z = x * y;
+  document.getElementById("cabecera").innerHTML = z;
+  console.log(z);
+  window.alert(z);
+  document.write(z);
+}
