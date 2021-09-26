ae7158ee4e3e655e43f49e2928748145fd47627a
Inma Gijón <igijoninf@gmail.com>
2021-09-26 19:00:57 +0200

    Algunos ejemplos de la ud4. Funciones, ámbito, invocación.

---
+++ 4_Estructuras_definidas_usuario_Funciones_Arrays_POO_Herencia/ej3_function/js/function.js
@@ -0,0 +1,14 @@
+var parImpar = (function () {
+  var fecha = new Date();
+  if (fecha.getDate() % 2 == 0) {
+    return function () {
+      document.write("La fecha es día par");
+    };
+  } else {
+    return function () {
+      document.write("La fecha es día impar");
+    };
+  }
+})();
+
+parImpar();
